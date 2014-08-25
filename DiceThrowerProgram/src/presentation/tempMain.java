166274fb5764ff45c938f31b9f44e1bfd1260570
Thomas Hedegaard <thomashj93@gmail.com>
2014-08-25 21:37:41 +0200

    Added class in Pres
    - Added temp main

---
+++ DiceThrowerProgram/src/presentation/tempMain.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package presentation;
+
+/**
+ *
+ * @author ThomasHedegaard
+ */
+public class tempMain {
+    public static void main(String[] args) {
+        System.out.println("only temporary!");
+    }
+}
