cbafb0c04450e4d2de666e18051432d11b93f3b7
Thomas Hedegaard <thomashj93@gmail.com>
2014-08-25 20:28:37 +0200

    Program setup
    - Made project DiceThrowerProgram
    - Made packages
    - Made classes: Controller, Test

---
+++ DiceThrowerProgram/src/logic/Test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package logic;
+
+/**
+ *
+ * @author ThomasHedegaard
+ */
+public class Test {
+    // Make a thread from this class!
+    
+    
+    
+    
+    
+}
